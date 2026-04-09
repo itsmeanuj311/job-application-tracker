da70fd477847f258d259f4db40e79a62f9510266
itsmeanuj311 <rockanuj311@gmail.com>
2026-04-09 22:49:55 +0530

    login page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/register/page.tsx
@@ -0,0 +1,20 @@
+import { Briefcase } from "lucide-react"
+import Link from "next/link";
+
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/register" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Briefcase className="size-4" />
+          </div>
+          Job Tracker
+        </Link>
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
